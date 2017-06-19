148b4a8548f61625d6f2d562c17aee46a0a6b3f7
zoe-c <find.zoec@gmail.com>
2017-06-19 15:26:01 -0400

    keep trying at hardmode. 2 attempts commented out

--- index.js
+++ index.js
@@ -26,6 +26,36 @@ app.get('/lorem', function (req, res) {
    res.send(output)
 });
 
+//failed attempt 1 at hardmode VVVV... veesh.
+// var num = [i <= 50];
+// var countUndefined = loremIpsum({
+//    count: parseInt(num);
+//    , units: 'paragraphs'
+//    , sentenceLowerBound: 5
+//    , sentenceUpperBound: 15
+//    , paragraphLowerBound: 3
+//    , paragraphUpperBound: 5
+//    , format: 'html'
+//    , suffix: "ZING"
+// });
+
+//failed attempt 2 at hardmode VVVVV...VEESH.
+// app.get('/lorem/:number' function (req, res) {
+//    let req.params.number = parseInt(int)
+//    res.send(loremIpsum ({
+//       count:
+//       , units: 'paragraphs'
+//       , sentenceLowerBound: 5
+//       , sentenceUpperBound: 15
+//       , paragraphLowerBound: 3
+//       , paragraphUpperBound: 5
+//       , format: 'html'
+//       // , words: ['ad', 'dolor', ... ]
+//       // , random: Math.random
+//       , suffix: "ZING"
+//    }));
+
+
 app.listen(3000, function () {
   console.log('Voila!')
 });
